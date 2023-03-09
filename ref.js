a63ba5b3d2248e8050610dd02670588501af5d31
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-03-08 18:32:55 -0600

    Add files via upload

---
+++ ref.js
@@ -0,0 +1,5 @@
+//start
+console.log("Ello Govna");//put this at the back bc the parser is top-bottem
+function Button(){
+    alert("This has been clicked");
+}
---
+++ warmup.js
@@ -0,0 +1,26 @@
+//this is some warm ups, also single
+/*Multi */
+//<!-this is html -->
+/*We got bool(lol mayb) */
+
+//bigints: 17n(floats and doubles)
+//strings are not real
+//`How is this a valid syntax: ${256} and ${true}`
+console.error("We got a problem");
+//asserts.equal(7+ 1,8);//assertion. it has to be this or the comp gone shit itself
+//asserts.equal(true && false, false);
+
+const x = 'This is a constant';//this is a constant(Immutatble py taught me that)
+
+let y,a,b; //this is an undeclared var. 
+
+y = 5 + 8;//now its declared
+b = 6;
+a += 1;// Idk its c based so ya know.
+
+function WAZZUP(a,b){
+    a + b;
+    alert('WAZZZZZUp');
+}
+
+
