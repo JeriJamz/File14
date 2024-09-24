a645e197b2195e1b55e52b97c4f08a5c663fca76
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-09-24 17:08:24 -0500

    Create Ml.py

---
+++ Ml.py
@@ -0,0 +1,22 @@
+#Slowly
+
+class centage:
+
+
+    __init__(self,bullseye,percise,accurate,shots):
+        self.bullseye = int(range(accuraccy,11))
+        self.percise = percise
+        self.accurate = accurate
+        self.shots = [] #might add an .append method so it can act like a magazine
+
+    def accuraccy(self,percise,accurate):
+
+        match range(self.shots in self.bullseye):#this case things works i just don't
+            case self.percise not in range(int(bullseye)) or self.accurate not in range(int(bullseye)):
+                print("Neither Accurate or Percise")
+            case self.shots is in range(self.percise(0,2)) and self.accurate != self.bullseye:
+                print("Percise not Accurate")
+            case self.accurate == self.bullseye and self.shots not in range(self.percise(0,2)):
+                print("Accurate not Percise")
+            case self.accurate == self.bullseye and self.accurate == self.bullseye:
+                print("BULLSEYE")
